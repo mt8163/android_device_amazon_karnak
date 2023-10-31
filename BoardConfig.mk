008d91fb6681544507fa0a69eea144aabe977b62
Kai Jones <19639817+488315@users.noreply.github.com>
2023-10-31 16:54:12 -0700

    karnak: define Bootloader and Platform flags
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,12 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 TARGET_USES_64_BIT_BINDER := true
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := karnak
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt8163
+
 # Inherit the proprietary files
 include vendor/amazon/karnak/BoardConfigVendor.mk
