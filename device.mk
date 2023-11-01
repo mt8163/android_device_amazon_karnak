4d0e87d7f5f1b5e0bc99288d54b4cccfb364601b
Kai Jones <19639817+488315@users.noreply.github.com>
2023-10-31 22:21:00 -0700

    karnak: Configure Gatekeeper stack
    Change-Id: Icdf22e29aceb9acea0fc02981d872b308a01396f
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libdrm.vendor
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
