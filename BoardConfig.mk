7673742688f468f1534dec6d3c27d017f0686ef5
Michael Bestas <mkbestas@lineageos.org>
2023-10-31 17:18:39 -0700

    karnak: Set androidboot.init_fatal_reboot_target=recovery
    Change-Id: Ia85432db40e1fa42cab198943fbab677a6d4d409
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ BOARD_MKBOOTIMG_ARGS := \
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Platform
 TARGET_BOARD_PLATFORM := mt8163
