37c9870e9faeca8ea8ad5b457356d594ea57b03a
Randall Huang <huangrandall@google.com>
2023-11-01 18:06:35 -0700

    karnak: Enable partitions on loop devices.
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.
    
    Bug: 116062201
    Test: builds, boots
    
    Change-Id: I393b41b3f3c4ee2a01877595240c60e85010d896
    Signed-off-by: Randall Huang <huangrandall@google.com>
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ BOARD_MKBOOTIMG_ARGS := \
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Build System
