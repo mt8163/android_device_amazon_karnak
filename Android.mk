1676d529ffaab49b4800b727d920d9f6299455bf
Kai Jones <19639817+488315@users.noreply.github.com>
2023-11-01 17:11:40 -0700

    karnak:symlink nvdata to /data/nvram
    * this is for modern nvram libs.
    
    Change-Id: I282629dba44a9d92f1b74fd069e99cc2821b8fa1
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter karnak, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+NVDATA_SYMLINK := $(TARGET_OUT_VENDOR)/nvdata
+$(NVDATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "nvdata link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/vendor/nvram $@
+
 endif
