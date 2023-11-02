58ddddb604c496a84c9661a24bb2efba3e5b8a4c
Kai Jones <19639817+488315@users.noreply.github.com>
2023-11-01 18:25:31 -0700

    karnak: build `android.hardware.power@1.2`
    Change-Id: Ia0239b8056b390f68b01cd4d31715b0e2acc0c61
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -170,6 +170,7 @@ PRODUCT_PACKAGES +=\
     android.hardware.power@1.0-service \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.1.vendor \
+    android.hardware.power@1.2.vendor \
     power.mt8163
 
 # Rootdir
