01ac18c308dde5019c68667c4dad9907551d49d6
Kai Jones <19639817+488315@users.noreply.github.com>
2023-12-11 15:09:14 -0800

    karnak: inherit `common_full_tablet_wifionly.mk` if it exists
    Change-Id: I737c47d01a2c973504a85be6cee59b5dcaed1ea6
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/amazon/karnak/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product-if-exists, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit GApps
 $(call inherit-product-if-exists, vendor/vendor_gms/gms_full_tablet_wifionly.mk)
