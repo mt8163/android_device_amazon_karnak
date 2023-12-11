272173b34f15b9e17a55e9874ef1849ee847c780
Kai Jones <19639817+488315@users.noreply.github.com>
2023-12-11 15:17:11 -0800

    karnak: fix extract-files typo in `${PATCHELF}"
    it caused a lib to not to be patched, it was originally at {$PATCHELF}
    so i fixed it to ${PATCHELF}
    
    Change-Id: I841886c82207ccea9096445d422f5998eb624608
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libshim_gui.so" "${2}"
             ;;
         vendor/lib/libMtkOmxVdecEx.so)
-            "{$PATCHELF}" --add-needed "libshim_gui.so" "${2}"
+            "${PATCHELF}" --add-needed "libshim_gui.so" "${2}"
             ;;
         vendor/lib/hw/fireos.hardware.audio@4.0-impl.so)
             "${PATCHELF}" --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util_v28.so" "${2}"
