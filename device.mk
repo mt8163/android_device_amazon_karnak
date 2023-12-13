83a27649fed2611ad58fa82e33ee4eb666cb111b
Kai Jones <19639817+488315@users.noreply.github.com>
2023-12-13 00:48:35 -0800

    karnak: drm: switch to aidl drm service
    Change-Id: Id23a1ff59ea6c8bc2df700955d1945930fed742e
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -97,12 +97,14 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     libmockdrmcryptoplugin \
-    libdrmclearkeyplugin
+    libdrmclearkeyplugin 
 
 # Lights
 PRODUCT_PACKAGES += \
